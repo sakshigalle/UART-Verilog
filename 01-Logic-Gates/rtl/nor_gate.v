52f4e612cffd9ce927359f30efd485e35246ae4d
Sakshi Galle <sakshigalle@gmail.com>
2026-07-25 00:49:47 +0530

    Implemented NOR gate

---
+++ 01-Logic-Gates/rtl/nor_gate.v
@@ -0,0 +1,9 @@
+module nor_gate (
+    input A,
+    input B,
+    output Y
+);
+
+assign Y = ~(A | B);
+
+endmodule
---
+++ 01-Logic-Gates/tb/nor_gate_tb.v
@@ -0,0 +1,31 @@
+`timescale 1ns/1ps
+
+module nor_gate_tb;
+
+reg A;
+reg B;
+wire Y;
+
+nor_gate uut (
+    .A(A),
+    .B(B),
+    .Y(Y)
+);
+
+initial begin
+
+    $dumpfile("nor_gate.vcd");
+    $dumpvars(0, nor_gate_tb);
+
+    $display("A B | Y");
+    $monitor("%b %b | %b", A, B, Y);
+
+    A = 0; B = 0;
+    #10 A = 0; B = 1;
+    #10 A = 1; B = 0;
+    #10 A = 1; B = 1;
+    #10 $finish;
+
+end
+
+endmodule
